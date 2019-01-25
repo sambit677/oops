74aea61c8089e764697828db6bee9e6ec60b222c
sambit677 <sambit677@gmail.com>
2019-01-25 15:05:46 +0530

    k
    pk

--- InheritanceConfusion.cs
+++ InheritanceConfusion.cs
@@ -9,13 +9,17 @@ namespace Opps
 
        public InheritanceConfusion()
         {
+
+            A objOrA = new A();
+            objOrA.methodA(); // will call its own method
+
+
             A objA = new c();
 
-            //objA.methodA();
+            objA.methodA();  // it will call c class methodA as its overrided
             c objc =  new c();
-            objc.methodA();
+            objc.methodA();   // it will call its own ovverided method 
         }
-
 
     }
 
@@ -35,7 +39,6 @@ namespace Opps
            // base.methodA();
             Console.WriteLine("from Class B ");
         }
-
         public void methodB()
         {
 
@@ -45,12 +48,11 @@ namespace Opps
     class c : B
     {
 
-        //public override void methodA()
-        //{
-        //    //base.methodA();
-        //    Console.WriteLine("from Class C ");
-        //}
-
+        public override void methodA()
+        {
+            base.methodA();  // will call Class B methodA
+            Console.WriteLine("from Class C ");
+        }
         public void methodC()
         {
 
